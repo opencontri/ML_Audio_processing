b5834dc5c8401141ce993f7f05b2921f471306c4
MikeGillotti <38792737+MikeGillotti@users.noreply.github.com>
2022-10-16 14:46:53 -0600

    Update app.py
    Added libraries
    Added Generate and Play Audio buttons
    Adjusted generateAudio to load from prompt instead of lyrics.
    Started off generateText function

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import tkinter as tk
+from tkVideoPlayer import TkinterVideo
 import customtkinter as ctk
 import os 
 import torch
- 
+import vlc
+import torchaudio
+import IPython
+import soundfile
+from tortoise.api import TextToSpeech
+from tortoise.utils.audio import load_audio, load_voice, load_voices
+
 app = tk.Tk()
 app.geometry("600x550")
 app.title("Rap God v2.0")
@@ -18,11 +25,15 @@ prompt.pack(side='left', padx=10)
 lyrics = ctk.CTkEntry(height=240, width=500, text_font=("Arial", 20), text_color="black", fg_color="white")
 lyrics.pack()
 
+def generateText():	
+	if os.path.exists('generated.wav'):
+		 p = vlc.MediaPlayer('file:///generated.wav')
+		
 
 def generateAudio(): 
-    voice_samples, conditioning_latents = load_voice('stormzy', extra_voice_dirs=['stormzy_samples']) 
+    voice_samples, conditioning_latents = load_voice('stormzy', extra_voice_dirs=['stormzy_samples'])  
     tts = TextToSpeech()
-    gen = tts.tts_with_preset(lyrics.get(), voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset='ultra_fast') 
+    gen = tts.tts_with_preset(prompt.get(), voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset='ultra_fast') 
     torchaudio.save('generated.wav', gen.squeeze(0).cpu(), 24000) 
 
 def playAudio(): 
@@ -39,7 +50,10 @@ def generateVideo():
         p = vlc.MediaPlayer('file:///generated.wav') 
         p.play()
            
-
+genAudioButton =ctk.CTkButton(promptFrame,height=40, width=120, text_font=("Arial", 20), text_color="black", text="Generate Audio", command=generateAudio)
+genAudioButton.pack(side='right')
+playAudioButton =ctk.CTkButton(promptFrame,height=40, width=120, text_font=("Arial", 20), text_color="black", text="Play Audio", command=playAudio)
+playAudioButton.pack(side='right')
 genTextButton =ctk.CTkButton(promptFrame, height=40, width=120, text_font=("Arial", 20), text_color="black", text="Generate", command=generateText)
 genTextButton.pack(side='right')
  
